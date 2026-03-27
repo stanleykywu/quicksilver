4f3f181c10edc82e5960fa96db493c050138b9a1
Stanley Wu <stanleykywu@gmail.com>
2026-03-27 14:00:37 -0500

    update zip shell script

---
+++ scripts/create-chromium-zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+EXT_NAME="quicksilver-chromium-extension"
+OUT_ZIP="$EXT_NAME.zip"
+
+zip -r "$OUT_ZIP" \
+  chromium/manifest.json \
+  chromium/*.js \
+  chromium/*.html \
+  chromium/pkg \
+  chromium/LICENSE.md \
+  chromium/assets/icon*.png
